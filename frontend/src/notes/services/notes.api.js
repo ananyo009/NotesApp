8e057165db0d7a220294826426435555e70ac534
Ananyo mitra <ananyomitra310@gmail.com>
2026-07-11 12:40:41 +0530

    second commit

--- frontend/src/notes/services/notes.api.js
+++ frontend/src/notes/services/notes.api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/notesApp/notes",
-    withCredentials:true
+  baseURL: "https://notes-app-seven-lyart.vercel.app",
+  withCredentials: true,
 });
 
 export async function createnote(title,description){
